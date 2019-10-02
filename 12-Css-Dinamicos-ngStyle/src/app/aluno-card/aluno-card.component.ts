8a1ade736a10abcc37323b886ab4cea918c8fec8
Jadilson Guedes <jadilson12@gmail.com>
2019-10-02 05:38:32 -0300

    Estilos Css Dinanmicos com ngStyle

---
+++ 12-Css-Dinamicos-ngStyle/src/app/aluno-card/aluno-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-aluno-card",
+  templateUrl: "./aluno-card.component.html",
+  // styleUrls: ["./aluno-card.component.css"]
+  styles: [
+    `
+      .card-body {
+        text-transform: uppercase;
+        color: blue;
+      }
+    `
+  ]
+})
+export class AlunoCardComponent {
+  @Input() aluno: any;
+
+  getStyleCartao() {
+    return {
+      'border-width.px': this.aluno.id,
+      backgroundColor: this.aluno.id % 2 === 0 ? 'lightblue' : 'lightgreen',
+    };
+  }
+}
